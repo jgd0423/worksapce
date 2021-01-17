069e038b5c54bf00eb68b1b2f6afde29457a79d1
Jeong_geumdam <suseongland@gmail.com>
2021-01-17 19:32:00 +0900

    NewLecture java Collection 04 Wrapper class and Auto boxing/Unboxing

---
+++ CollectionPrj/src/com/newlecture/app/util/Program.java
@@ -0,0 +1,15 @@
+package com.newlecture.app.util;
+
+public class Program {
+	public static void main(String[] args) {
+		IntList list = new IntList();
+		list.add(3);
+		list.add(5);
+		list.clear();
+		int size = list.size();
+		System.out.printf("size : %d\n", size);
+		list.add(7);
+		int num = list.get(0);
+		System.out.printf("num : %d\n", num);
+	}
+}
