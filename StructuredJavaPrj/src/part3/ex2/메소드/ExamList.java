46877d8b1a08972a9bf38fc377ff50a84ee4af80
Jeong_geumdam <suseongland@gmail.com>
2021-04-28 02:54:10 +0900

    뉴렉처 자바 객체지향 강의 11강 - ExamConsole 클래스 구현하기

--- StructuredJavaPrj/src/part3/ex2/메소드/ExamList.java
+++ StructuredJavaPrj/src/part3/ex2/메소드/ExamList.java
@@ -1,7 +1,5 @@
 package part3.ex2.메소드;
 
-import java.util.Scanner;
-
 public class ExamList {
 	private Exam[] exams;
 	private int current;
@@ -14,83 +12,12 @@ public class ExamList {
 		exams = new Exam[size];
 		current = 0;
 	}
-	
-	public void printList() {
-    	printList(current);
-	}
     
-	public void printList(int size) {
-        System.out.println("┌───────────────────────────┐");
-        System.out.println("│           성적  출력          │");
-        System.out.println("└───────────────────────────┘");
-        System.out.println();
-        
-        for (int i = 0; i < size; i++) {
-	        Exam exam = this.get(i);
-	        int kor = exam.getKor();
-	        int eng = exam.getEng();
-	        int math = exam.getMath();
-	        
-	        int total = exam.total();
-	        float avg = exam.avg();
-	        
-	    	System.out.printf("국어 : %d\n", kor);
-	        System.out.printf("영어 : %d\n", eng);
-	        System.out.printf("수학 : %d\n", math);
-	        	        
-	        System.out.printf("총점 : %3d\n", total);
-	        System.out.printf("평균 : %6.2f\n", avg);
-	        System.out.println("─────────────────────────────");
-        }
-	}
-    
-	private Exam get(int i) {
+	public Exam get(int i) {
 		return this.exams[i];
 	}
 
-	public void inputList() {
-    	Scanner scan = new Scanner(System.in);
-		
-    	System.out.println("┌───────────────────────────┐");
-        System.out.println("│           성적  입력          │");
-        System.out.println("└───────────────────────────┘");
-        System.out.println();
-		
-        int kor, eng, math;
-        
-        do {
-	        System.out.printf("국어 : ");
-	        kor = scan.nextInt();
-        
-	        if (kor < 0 || 100 < kor)
-	        	System.out.println("국어성적은 0~100까지의 범위만 입력이 가능합니다.");
-	        
-        } while (kor < 0 || 100 < kor);
-        
-        do {
-	        System.out.printf("영어 : ");
-	        eng = scan.nextInt();
-        
-	        if (eng < 0 || 100 < eng)
-	        	System.out.println("영어성적은 0~100까지의 범위만 입력이 가능합니다.");
-	        
-        } while (eng < 0 || 100 < eng);
-        
-        do {
-	        System.out.printf("수학 : ");
-	        math = scan.nextInt();
-        
-	        if (math < 0 || 100 < math)
-	        	System.out.println("수학성적은 0~100까지의 범위만 입력이 가능합니다.");
-	        
-        } while (math < 0 || 100 < math);
-        
-        Exam exam = new Exam(kor, eng, math);
-        
-        add(exam);
-    }
-
-	private void add(Exam exam) {
+	public void add(Exam exam) {
         Exam[] exams = this.exams;
         int size = current;
         
@@ -104,6 +31,10 @@ public class ExamList {
         
         exams[current] = exam;
         current++;
-		
+	}
+
+
+	public int size() {
+		return current;
 	}
 }
