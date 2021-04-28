1766f8f3759ba1cfa656b880a7522593c0b81a2b
Jeong_geumdam <suseongland@gmail.com>
2021-04-28 18:27:00 +0900

    뉴렉처 자바 객체지향 강의 13강 - 코드 재사용이란?

---
+++ OOPJavaPrj/src/Program.java
@@ -0,0 +1,15 @@
+import part3.ex2.메소드.Exam;
+
+public class Program {
+	
+	// 재사용은 소스코드를 복붙하는게 아니라 바이너리를 재사용하는 것이다.
+	// STructuredJavaPrj의 part3.ex2.메소드 부분의 
+	// Exam.java, ExamConsole.java, ExamList.java를 jar로 Export한다.
+	// OOPJavaPrj에 build path에서 jar를 등록한다.
+	
+	public static void main(String[] args) {
+		Exam exam = new Exam(1, 1, 1);
+		
+		System.out.println(exam.total());
+	}
+}
