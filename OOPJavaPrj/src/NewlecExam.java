869a5c30f4969d84817fc75edbdd902e3eb284db
Jeong_geumdam <suseongland@gmail.com>
2021-04-29 14:29:10 +0900

    뉴렉처 자바 객체지향 강의 16강 - Override(우선순위가 높은) 메소드

--- OOPJavaPrj/src/NewlecExam.java
+++ OOPJavaPrj/src/NewlecExam.java
@@ -3,6 +3,16 @@ import part3.ex2.메소드.Exam;
 public class NewlecExam extends Exam {
 	private int com;
 	
+	@Override
+	public int total() {
+		return super.total() + com;
+	}
+	
+	@Override
+	public float avg() {
+		return total() / 4.0f;
+	}
+	
 	public int getCom() {
 		return com;
 	}
